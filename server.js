840f71e5b23c46bc709d9c5fa90b5226b6739ada
Pranjal Sharma <127693503+code-prsh@users.noreply.github.com>
2023-08-12 00:11:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,15 @@ import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import cors from 'cors'
 import path from 'path'
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 connectDB();
+
+const __filename=fileURLToPath(import.meta.url)
+
+const __dirname=path.dirname(__filename);
 const app = express();
 
 app.use(cors());
