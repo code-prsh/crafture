a8162617077dec8131cbbcabdfebc7c2a93f5b2b
Pranjal Sharma <127693503+code-prsh@users.noreply.github.com>
2023-08-23 19:20:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 app.use(morgan('dev'))
-app.use('*',express.static(path.join(__dirname, './client/build')))
+app.use('*',express.static(path.join(__dirname, "client","build")))
 
 app.use('/api/v1/auth',authRoutes);
 app.use('/api/v1/category',categoryRoutes);
