e3d14f5597a61b98446710dd4df47234b95707ba
Pranjal Sharma <127693503+code-prsh@users.noreply.github.com>
2023-08-23 19:18:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname, './client/build')))
+app.use('*',express.static(path.join(__dirname, './client/build')))
 
 app.use('/api/v1/auth',authRoutes);
 app.use('/api/v1/category',categoryRoutes);
@@ -40,5 +40,4 @@ const PORT = process.env.PORT || 8080;
     console.log('server running on: '.bgCyan.white + PORT.bgCyan.white);
 })
 
-expose 8080
 
